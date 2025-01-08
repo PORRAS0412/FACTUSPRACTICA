82ca6cf37e8214a103130830961ac67d4f2e9708
ANDRES JULIAN PORRAS <91100444+PORRAS0412@users.noreply.github.com>
2025-01-08 16:32:00 -0500

    first committ

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -7,44 +7,47 @@ import { AuthapisService } from '../services/authapis.service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthapisService);  // Inyección del servicio
 
-  // 1. Obtén el authToken y refreshToken del sessionStorage
-  const authToken = sessionStorage.getItem('authToken');
-  const refreshToken = sessionStorage.getItem('refreshToken'); // Asegúrate de tener el refresh_token en sessionStorage
+  // Verificar si estamos en el navegador antes de acceder a sessionStorage
+  if (typeof window !== 'undefined' && typeof sessionStorage !== 'undefined') {
+    // 1. Obtén el authToken y refreshToken del sessionStorage
+    const authToken = sessionStorage.getItem('authToken');
+    const refreshToken = sessionStorage.getItem('refreshToken'); // Asegúrate de tener el refresh_token en sessionStorage
 
-  // 2. Si el authToken existe, añade el encabezado Authorization
-  if (authToken) {
-    const authReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
-    return next(authReq);
-  }
+    // 2. Si el authToken existe, añade el encabezado Authorization
+    if (authToken) {
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      return next(authReq);
+    }
 
-  // 3. Si no existe el authToken pero hay un refreshToken, intenta obtener un nuevo authToken
-  if (refreshToken) {
-    return authService.refreshtoken(refreshToken).pipe(
-      switchMap((newTokenResponse) => {
-        // Si la respuesta es exitosa, guarda el nuevo authToken y refreshToken
-        sessionStorage.setItem('authToken', newTokenResponse.access_token); // Usa el nombre correcto de la respuesta
-        sessionStorage.setItem('refreshToken', newTokenResponse.refresh_token); // Guarda el nuevo refresh_token si es proporcionado
+    // 3. Si no existe el authToken pero hay un refreshToken, intenta obtener un nuevo authToken
+    if (refreshToken) {
+      return authService.refreshtoken(refreshToken).pipe(
+        switchMap((newTokenResponse) => {
+          // Si la respuesta es exitosa, guarda el nuevo authToken y refreshToken
+          sessionStorage.setItem('authToken', newTokenResponse.access_token); // Usa el nombre correcto de la respuesta
+          sessionStorage.setItem('refreshToken', newTokenResponse.refresh_token); // Guarda el nuevo refresh_token si es proporcionado
 
-        // Clona la solicitud original con el nuevo token y la pasa al siguiente interceptor
-        const clonedReq = req.clone({
-          setHeaders: {
-            Authorization: `Bearer ${newTokenResponse.access_token}`,
-          },
-        });
+          // Clona la solicitud original con el nuevo token y la pasa al siguiente interceptor
+          const clonedReq = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${newTokenResponse.access_token}`,
+            },
+          });
 
-        return next(clonedReq);
-      }),
-      catchError((error) => {
-        // Maneja el error si la solicitud de refreshToken falla
-        console.error('Error al refrescar el token:', error);
-        // Aquí puedes redirigir al usuario a la página de login si el refreshToken falla
-        return of(error);
-      })
-    );
+          return next(clonedReq);
+        }),
+        catchError((error) => {
+          // Maneja el error si la solicitud de refreshToken falla
+          console.error('Error al refrescar el token:', error);
+          // Aquí puedes redirigir al usuario a la página de login si el refreshToken falla
+          return of(error);
+        })
+      );
+    }
   }
 
   // Si no hay ningún token ni refreshToken, pasa la solicitud sin cambiar
